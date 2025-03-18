1acc7d601f61d1724c5980eef87e28a842e0a5bd
VictorMGCYT <manuelvgc2233@gmail.com>
2025-03-17 21:00:25 -0600

    Adding dependencies
    17-03-2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ 
+
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST, // Only localhost in this case
+      port: Number(process.env.DB_PORT) ?? 5433, // 5433
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // ! Don't use this in production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
